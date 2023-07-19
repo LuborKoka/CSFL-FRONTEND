1544bb6aff9256fead1a58db94d3c6d19fe61a6a
Ľubor Koka <koka.lubor87@gmail.com>
2023-07-19 22:55:03 +0200

    in season navigation

--- src/components/subcompontents/user/Race.tsx
+++ src/components/subcompontents/user/Race.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import '../../../styles/race.css';
-import { Link } from "react-router-dom";
+import { Link, useOutletContext } from "react-router-dom";
+import { RaceContext } from "../../controls/SeasonNav";
+
 
 type Props = {
     raceID: string,
@@ -11,12 +13,19 @@ type Props = {
 }
 
 export default function Race({ raceID, name, raceName, date, setMini }: Props) {
+    const setContext = (useOutletContext() as RaceContext)[1]
+    
+    function setRace() {
+        setContext(p => {
+            return {...p, raceName: raceName}
+        })
+    }
 
     return(
         <div className="race-box" onClick={() => setMini(p => !p)}>
             <h2>{raceName}</h2>
             <p>{date}</p>
-            <Link to={`race/${raceID}`}>View</Link>
+            <div onClick={setRace}><Link to={`race/${raceID}`}>View</Link></div>
         </div>
     )
 }
