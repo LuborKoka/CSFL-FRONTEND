932553381cd78aec6796e093309a2e0dd912c336
LuborKoka <koka.lubor87@gmail.com>
2023-08-15 23:11:07 +0200

    added discord link

--- src/components/screens/Settings.tsx
+++ src/components/screens/Settings.tsx
@@ -1,6 +1,7 @@
 import React, { Context, useContext, useRef, useState } from "react";
 import axios, { AxiosError, AxiosResponse } from 'axios'
 import { URI, UserContext, UserTypes } from "../../App";
+import { Link } from "react-router-dom";
 
 
 export default function Settings() {
@@ -44,6 +45,19 @@ export default function Settings() {
                 <input ref={newPasswordConfirm} placeholder="Potvrď nové heslo" type="password" />
                 <button disabled={isPending} type="submit">Zmeniť heslo</button>
             </form>
+
+
+            <h2 className="section-heading fade-in-out-border">
+                Prepojiť účet s Discordom
+            </h2>
+
+            <Link to={'https://discord.com/api/oauth2/authorize?client_id=1140670852166328411&redirect_uri=http%3A%2F%2F192.168.100.22%3A3000%2Fverify-user&response_type=code&scope=identify%20guilds.members.read'}>
+                Prihlásiť
+            </Link>
         </div>
     )
 }
+
+
+//  http://192.168.100.22:3000/seasons/e40897ad-64dd-44ac-8cbc-f62615663e15/race/554f52d4-4459-4047-82f1-997d0ba25ec8/reports
+//  http://192.168.100.22:3000/seasons/e40897ad-64dd-44ac-8cbc-f62615663e15/races/554f52d4-4459-4047-82f1-997d0ba25ec8/reports
